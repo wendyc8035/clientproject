c9958ff2979af20d347b1df135d9d991e28e7a32
wendyc8035 <wendyc8035@queensvoc.org>
2019-05-16 22:32:28 +0000

    day 1

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ $('h1').click(function(){
     $('h1').css('color', 'green');
     $('h1').text('Ready to Code');
 });
+
+   $('#searchButton').click(function() {
+        var searchTerm = $("#searchTerm").val();
+        var giphyUrl = "https://api.soundcloud.com/tracks?q=" + searchTerm + "&client_id=5aa8e389ba4e24b6106af5159ab3e344";
+
+
+        $.ajax({
+          url: giphyUrl,
+          method: "GET",
+          success: function(response) {
+              var songTitle = response[0].title;
+              console.log(songTitle);
+               var songs = "<h1>" + songTitle + " </h1>";
+
+              $("body").append(songs);
+          }
+        })
+   });
